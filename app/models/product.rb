0edcba320e292b524c2bef5e059423d7797934df
Pedro Paiva <pedro.v.s.paiva@gmail.com>
2020-02-16 12:53:22 -0300

    Iteration G2: Atom Feeds

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,10 +8,13 @@
 #---
 class Product < ApplicationRecord
   has_many :line_items
+  has_many :orders, through: :line_items
+  #...
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
   validates :title, :description, :image_url, presence: true
+# 
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
     with:    %r{\.(gif|jpg|png)\z}i,
@@ -23,11 +26,11 @@ class Product < ApplicationRecord
 
   private
 
-  # ensure that there are no line items referencing this product
-  def ensure_not_referenced_by_any_line_item
-    unless line_items.empty?
-      errors.add(:base, 'Line Items present')
-      throw :abort
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
     end
-  end
 end
