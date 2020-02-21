4529d0155e2e6e2f8280cc047a63a2a33b3102de
Pedro Paiva <pedro.v.s.paiva@gmail.com>
2020-02-20 22:38:25 -0300

    Iteration J1: Adding Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,7 @@
 # We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/rails6 for more book information.
 #---
-Rails.application.routes.draw do
-  resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
-  resources :products do
-    get :who_bought, on: :member
-  end
-
-  # For details on the DSL available within this file, see
-  # https://guides.rubyonrails.org/routing.html
+class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  has_secure_password
 end
