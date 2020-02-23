19cc580b9acbc2a9490f30fe197c68414a9a2616
Pedro Paiva <pedro.v.s.paiva@gmail.com>
2020-02-23 15:54:36 -0300

    Iteration L2: Storing Support Requests from Our Mailbox

--- app/models/support_request.rb
+++ app/models/support_request.rb
@@ -6,17 +6,6 @@
 # We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/rails6 for more book information.
 #---
-class SupportMailbox < ApplicationMailbox
-  def process
-    recent_order = Order.where(email: mail.from_address.to_s).
-                         order('created_at desc').
-                         first
-
-    SupportRequest.create!(
-      email:   mail.from_address.to_s,
-      subject: mail.subject,
-      body:    mail.body.to_s,
-      order:   recent_order
-    )
-  end
+class SupportRequest < ApplicationRecord
+  belongs_to :order, optional: true
 end
